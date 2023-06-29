a56b0a95636f67614c8356c02eff4ebb774f730b
Yelk11 <neighbour.matthew@gmail.com>
2023-06-29 08:53:25 -0400

    moved the gitignore file

--- utils/csv_io.py
+++ utils/csv_io.py
@@ -4,13 +4,23 @@ import csv
 class CSV_IO:
     __init__(self, file_name):
         self._file_name = file_name
+        self._file
 
-
-    def write_string(string):
+    def open(self):
+        with open(self._file_name, 'w', newline='') as file:
+            self._file = file
+    
+    def close(self):
         pass
 
+    def write_string(self, string):
+        writer = csv.writer(file)
+        writer.writerow([string])
+            
 
-    def read_string_by_row(row_number):
+
+
+    def read_string_by_row(self, row_number):
         pass
 
 
