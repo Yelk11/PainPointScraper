2da8c22c610716865174301a1e8714d4af2bba05
Yelk11 <neighbour.matthew@gmail.com>
2023-06-29 08:44:36 -0400

    made csv io a class

--- utils/csv_io.py
+++ utils/csv_io.py
@@ -1,12 +1,17 @@
 import csv
 
 
-def write_string(string):
-    pass
+class CSV_IO:
+    __init__(self, file_name):
+        self._file_name = file_name
 
 
-def read_string_by_row(row_number):
-    pass
+    def write_string(string):
+        pass
+
+
+    def read_string_by_row(row_number):
+        pass
 
 
 
